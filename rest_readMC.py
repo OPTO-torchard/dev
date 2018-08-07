de1777d710fa10b4d3ee9a8292b778209f7a5dda
Terry Orchard <torchard@opto22.com>
2018-08-07 20:09:08 +0000

    added status code catch

--- rest_readMC.py
+++ rest_readMC.py
@@ -6,14 +6,19 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 # head is a JSON object that holds API key information
 head = {    'apiKey' : 'MDKu7n6PQENni3goRuQjvTQJGD3UBCYp',
             'Content-Type' : 'application/json' }
-host = '10.192.1.4'
+host = 'localhost'
 mod = sys.argv[1] # 1st argument = module number
 chn = sys.argv[2] # 2nd argument = channel number
 # construct request URL for the 'local' built-in rack of I/O *status*
 url = 'https://'+host+'/manage/api/v1/io/local/modules/'+mod+'/channels/'+chn+'/digital/status'
 # make the RESTful get request and save the response
 response = requests.get(url, headers=head, verify=False)
-# find the state result in the response text, then output it
-start = response.text.index('state') + 7
-end = response.text.index(',', start)
-print response.text[start:end]
+# if the request failed, alert the user and exit the script
+if response.status_code != 200:
+    print('Read request failed.')
+    sys.exit()
+# otherwise find the state result in the response text, then output it
+else:
+    start = response.text.index('state') + 7
+    end = response.text.index(',', start)
+    print response.text[start:end]
