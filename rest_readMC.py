c4db5d529fab00af8a7509e0cfeaa2024676d2ad
Terry Orchard <torchard@opto22.com>
2018-08-07 13:00:59 -0700

    fixed output

--- rest_readMC.py
+++ rest_readMC.py
@@ -6,15 +6,14 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 # head is a JSON object that holds API key information
 head = {    'apiKey' : 'MDKu7n6PQENni3goRuQjvTQJGD3UBCYp',
             'Content-Type' : 'application/json' }
+host = '10.192.1.4'
 mod = sys.argv[1] # 1st argument = module number
 chn = sys.argv[2] # 2nd argument = channel number
 # construct request URL for the 'local' built-in rack of I/O *status*
-url = 'https://opto-03-f6-41/manage/api/v1/io/local/modules/'+mod+'/channels/'+chn+'/digital/status'
-print('Reading value at output ' + chn + ' on module ' + mod)
+url = 'https://'+host+'/manage/api/v1/io/local/modules/'+mod+'/channels/'+chn+'/digital/status'
 # make the RESTful get request and save the response
 response = requests.get(url, headers=head, verify=False)
-# output the result:
-print (response)
-print (response.content)
-print (response.text)
-print (response.json)
+# find the state result in the response text, then output it
+start = response.text.index('state') + 7
+end = response.text.index(',', start)
+print response.text[start:end]
