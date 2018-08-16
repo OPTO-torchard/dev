9b4dbd4a4fb5dee4d98dc63ec01b49aaa812e259
Terry Orchard <torchard@opto22.com>
2018-08-16 21:31:14 +0000

    . . .

--- wip.py
+++ wip.py
@@ -1,50 +1,52 @@
+## use: >> python eRead.py <#> <#>
+##      <#> first argument = module number
+##      <#> second argument = channel number
 import sys
 import socket
 import struct
+import O22SIOUT
 
-host = '10.192.58.10' # SNAP PAC Learning Center
-#host = '10.192.0.201' # groov EPIC
+def connect(host):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host,2001))
+    return s
+
+def getEpicDig(module, channel):
+    dest = O22IOUT.
+
+host = '127.0.0.1' # localhost when ran from groov controller command line
 
 port = 2001 #MMP default port: 2001
 tcode = 5   # read block request
-modN = 0
-chN = 1
+modN = int(sys.argv[1]) # first argument / parameter
+chN = int(sys.argv[2])  # second argument / parameter
 
-#create and connect to the socket at host:port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
-dest = 0xF0800000 + (modN * 0x1000) + (chN * 0x40)
-print hex(dest)
-
-print str(hex(dest))[8:10] + ', ' + str(hex(dest))[6:8] + ', ' + str(hex(dest))[4:6] + ', ' + str(hex(dest))[2:4]
-
-print str(int(str(hex(dest))[8:10], 16)) + ', ' + str(int(str(hex(dest))[6:8], 16)) + ', ' + str(int(str(hex(dest))[4:6], 16)) + ', ' + str(int(str(hex(dest))[2:4], 16))
+##create and connect to the socket at host:port
+grvEpic = connect('127.0.0.1')
 
 
-print str(hex(64)) + ', ' + str(hex(0)) + ', ' + str(hex(128)) + ', ' + str(hex(240))
+
+##Calculate the destination offset:
+dest = 0xF01E0000 + (modN * 0x1000) + (chN * 0x40)
 
 tc = tcode << 4
-#build the block request: 
-#          dest  tl_ tc_  src  destoffset..............  len.. exttc
-#myBytes = [0, 0,  1, tc, 0, 0, 255, 255, 240, 48, 1, 12, 0, 16, 0, 0];
+##build the block request: 
+##  [_,_, tlabel_, tc_, src,src, Dest,Dest,  D.4,D.3,D.2,D.1,  len,len,  ext.tc,ext.tc]
 myBytes = [0, 0, 1, tc, 0, 0, 255, 255, int(str(hex(dest))[2:4],16), int(str(hex(dest))[4:6],16), int(str(hex(dest))[6:8],16), int(str(hex(dest))[8:10],16), 0, 16, 0, 0];
 
-
-print '# of bytes sent: ' + str(s.send(bytearray(myBytes)))
+##send the block request and save the response:
+nSent = s.send(bytearray(myBytes))
 data = s.recv(20)
-#upper and lower range of the data block in the package:
+
+##upper and lower range of the data block in the package:
 up = 20
 lo = 16
-#confirm block length and print raw data:
-print 'length: ' + str(len(data[lo:up])) + ', raw: ' + data[lo:up]
-
 #Big Endian reordering of the returned bytes:
 retBytes = []
 for i in range(up-lo):
     retBytes.append(data[up-1-i])
-#print the raw and then unpacked bytes:
-print 'length: ' + str(len(retBytes)) + ', raw: ' + str(retBytes)
-print 'unpack: ' + str(struct.unpack_from('i', bytearray(retBytes))) + '\n'
+result = str(struct.unpack_from('i', bytearray(retBytes)))
+print 'module ' + str(modN) + ', point ' + str(chN) + ' = ' + str(result)[1:-2]
 
 #close the socket:
 s.close()
